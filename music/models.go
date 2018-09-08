8af4b7ad495d23d2f4f4563bc6ec987eddcc7525
Joey Mandarino <joey@leotta.com>
2018-09-08 12:03:28 -0400

    update model structure

--- music/models.go
+++ music/models.go
@@ -13,11 +13,20 @@ Author string `json:"author" bson:"author"`
 Edited time.Time `json:"edited" bson:"edited"`
 }
 
+type version struct {
+	ID bson.ObjectId `json:"id" bson:"_id"`
+	ChangeType string `json:"changeType" bson:"changeType"`
+	Songs []bson.ObjectId `json:"songs" bson:"songs"`
+	Edited time.Time `json:"edited" bson:"edited"`
+}
+
+
 type playlist struct {
 ID bson.ObjectId `json:"id" bson:"_id"`
 UserId string `json:"userId" bson:"userId"`
 PlaylistId string `json:"playlistId" bson:"playlistId"`
 Songs []bson.ObjectId `json:"songs" bson:"songs"`
+Versions []bson.ObjectId `json:"versions" bson:"versions"`
 }
 
 type Playlists []playlist
---
+++ readme.md
@@ -0,0 +1,17 @@
+Plist
+songs []songs
+versions []v_id
+id
+spot_id
+
+songs
+id
+spot_id
+...
+
+version
+Change: (add|remove|)
+songs []song_id
+date
+
+TODO: better structure for tests
